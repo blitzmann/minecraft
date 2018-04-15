9b6e90c68483c44ddb326afc74a16e20e276ec40
blitzmann <holmes.ryan.90@gmail.com>
2018-04-14 23:21:30 -0400

    all the things!

---
+++ overviewerConfig.js
@@ -0,0 +1,65 @@
+var overviewerConfig = {
+    "worlds": [
+        "New World"
+    ], 
+    "tilesets": [
+        {
+            "spawn": [
+                153, 
+                65, 
+                254
+            ], 
+            "isOverlay": false, 
+            "last_rendertime": 1523759968, 
+            "name": "Normal Render of My World", 
+            "poititle": "Markers", 
+            "north_direction": 0, 
+            "minZoom": 0, 
+            "bgcolor": "#1a1a1a", 
+            "zoomLevels": 8, 
+            "base": "", 
+            "imgextension": "png", 
+            "defaultZoom": 1, 
+            "world": "New World", 
+            "maxZoom": 8, 
+            "path": "normalrender", 
+            "showlocationmarker": true
+        }
+    ], 
+    "CONST": {
+        "mapDivId": "mcmap", 
+        "UPPERLEFT": 0, 
+        "tileSize": 384, 
+        "UPPERRIGHT": 1, 
+        "image": {
+            "defaultMarker": "signpost.png", 
+            "queryMarker": "https://google-maps-icons.googlecode.com/files/regroup.png", 
+            "signMarker": "signpost_icon.png", 
+            "spawnMarker": "https://google-maps-icons.googlecode.com/files/home.png", 
+            "bedMarker": "bed.png"
+        }, 
+        "LOWERRIGHT": 2, 
+        "LOWERLEFT": 3, 
+        "regionStrokeWeight": 2
+    }, 
+    "map": {
+        "debug": true, 
+        "north_direction": "lower-left", 
+        "controls": {
+            "spawn": true, 
+            "mapType": true, 
+            "compass": true, 
+            "coordsBox": true, 
+            "overlays": true, 
+            "searchBox": true, 
+            "zoom": true, 
+            "pan": true
+        }, 
+        "center": [
+            -314, 
+            67, 
+            94
+        ], 
+        "cacheTag": "1523761957"
+    }
+};
